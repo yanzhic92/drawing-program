a7269f5639080a9ea537644c6cb0b7eea352cd56
yanzhic92 <zhic92y@gmail.com>
2015-12-02 14:09:55 -0500

    the constructor of the project

--- Drawing program/src/Drawing_window.java
+++ Drawing program/src/Drawing_window.java
@@ -1,5 +1,35 @@
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Drawing_window{
+
+public class Drawing_window extends JFrame {
+	private JMenuBar bar;
+	private JMenu file;
+	private JMenuItem newfile,openfile,savefile,exit;//items in "file"
+	private JToolBar buttons;
+	private DrawArea drawarea;
+	
+	public Drawing_window(int h, int w){
+		
+		file = new JMenu("File");
+		bar = new JMenuBar();
+		bar.add(file);
+		
+		setJMenuBar(bar);
+		
+		file.add(newfile);
+		file.add(openfile);		
+		file.add(savefile);		
+		file.add(exit);
+		
+		
+	    setSize(w, h);
+	    Container c = getContentPane();
+	    c.add(buttons, BorderLayout.NORTH);
+	    
+	    setVisible(true);
+	
+	}	
 	
 }
