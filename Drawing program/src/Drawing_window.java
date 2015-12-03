434fbfa5c0ba0744590b00fdeeaf5d04e13b8b8a
yanzhic92 <zhic92y@gmail.com>
2015-12-02 21:37:23 -0500

    add buttons

--- Drawing program/src/Drawing_window.java
+++ Drawing program/src/Drawing_window.java
@@ -7,16 +7,21 @@ public class Drawing_window extends JFrame {
 	private JMenuBar bar;
 	private JMenu file;
 	private JMenuItem newfile,openfile,savefile,exit;//items in "file"
-
+	
+	private JPanel buttonbar;
+	private JButton button[];
+	private String buttonnames[] = {
+			"pen","line","rect","circle","rubber"
+	};
 	
 	public Drawing_window(int w,int h){
+		//set menu
 		file = new JMenu("File");
 		bar = new JMenuBar();
 		bar.add(file);
 		
 		setJMenuBar(bar);
 		
-        
         newfile = new JMenuItem("New");  
         openfile = new JMenuItem("Open" );  
         savefile = new JMenuItem("Save");  
@@ -27,11 +32,21 @@ public class Drawing_window extends JFrame {
 		file.add(savefile);		
 		file.add(exit);
 		
+		//set buttons
+		buttonbar = new JPanel();
+		buttonbar.setLayout(new GridLayout(1,5));
+		button = new JButton[buttonnames.length];
+		   for(int i = 0; i < buttonnames.length; i++){
+			   button[i] = new JButton();
+			   buttonbar.add(button[i]);
+		   }
+		
 		setSize(w, h);
 		
 		DrawArea DrawArea = new DrawArea();
 	    Container c = getContentPane();
-	    c.add(DrawArea,BorderLayout.CENTER);  
+	    c.add(DrawArea,BorderLayout.CENTER);
+	    c.add(buttonbar,BorderLayout.NORTH);
 
 	    
 	    setVisible(true);
