e8332c17fa3aec739e31c745955a22f4ce948a1e
Juncheng Li <jli98@stevens.edu>
2015-12-04 14:42:25 -0500

    add MouseListener A B

--- Drawing program/src/MouseA.java
+++ Drawing program/src/MouseA.java
@@ -1,4 +1,35 @@
+import java.awt.event.*;
 
-public class MouseA {
+public class MouseA implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Drawing program/src/MouseB.java
+++ Drawing program/src/MouseB.java
@@ -1,4 +1,16 @@
+import java.awt.event.*;
 
-public class MouseB {
+public class MouseB implements MouseMotionListener {
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
